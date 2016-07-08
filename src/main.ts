2ca39120815dadd1bdf64a06de8de5310b19fa3b
Jakob McClanahan <jmcclanahan18@gmail.com>
2016-07-07 20:47:54 -0500

    Add angular http

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@
 
 import { bootstrap } from '@angular/platform-browser-dynamic'
 import { enableProdMode } from '@angular/core'
+
+import { HTTP_PROVIDERS } from '@angular/http'
 import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 import { AppComponent } from './app/app.component'
@@ -11,6 +13,7 @@ if (process.env.ENV === 'production') {
 }
 
 bootstrap(AppComponent, [
+  HTTP_PROVIDERS,
   disableDeprecatedForms(),
   provideForms()
 ])
