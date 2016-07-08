df29a4cd125877d8c871c5e5e8d9714421a8dda2
Jakob McClanahan <jmcclanahan18@gmail.com>
2016-07-07 20:48:13 -0500

    Disable rxjs for now

--- src/vendor.ts
+++ src/vendor.ts
@@ -2,7 +2,6 @@
 import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
-import '@angular/common';
 import '@angular/http';
 import '@angular/router';
 import '@angular/forms';
@@ -24,6 +23,6 @@ import '@angular2-material/tabs';
 import '@angular2-material/toolbar';
 
 // RxJS
-import 'rxjs';
+//import 'rxjs';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
