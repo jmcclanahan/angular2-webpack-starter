c25f0fc714fafb233ff33902fc6961d9b35e3df0
Jakob McClanahan <jmcclanahan18@gmail.com>
2016-07-13 12:42:50 -0500

    Update to es6
    Use spread operator in place of slice.apply and call for easier to read
    and understand code

--- config/helpers.js
+++ config/helpers.js
@@ -3,8 +3,8 @@ var path = require('path');
 var _root = path.resolve(__dirname, '..');
 
 function root(args) {
-  args = Array.prototype.slice.call(arguments, 0);
-  return path.join.apply(path, [_root].concat(args));
+  args = [...arguments];
+  return path.join(_root, ...args)
 }
 
 exports.root = root;
